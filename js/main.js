5fd2ddefe79c30769f0ce2fe74998e7a1c81b587
IstiantoIlham <istianto.pahlawan@gmail.com>
2022-10-13 14:11:04 +0700

    This is my commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const submitForm = document.getElementById('overlay-form');
-    submitForm.addEventListener('submit', function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+    const submitForm = document.getElementById("overlay-form");
+    submitForm.addEventListener("submit", function (event) {
         event.preventDefault();
         addBook();
         removeInput();
@@ -12,11 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function removeInput() {
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    document.getElementById('year').value = '';
-    document.getElementById('complete').checked = false;
-    document.getElementById('overlay-form').style.display = 'none';
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("year").value = "";
+    document.getElementById("complete").checked = false;
+    document.getElementById("overlay-form").style.display = "none";
 }
 
 document.addEventListener("ondatasaved", () => {
@@ -27,22 +27,26 @@ document.addEventListener("ondataloaded", () => {
     refreshDataFromBooks();
 });
 
-
 /* Overlay Form */
 
-const openForm = document.getElementById('open-form');
-const closeForm = document.getElementById('close-form');
-const bookCancle = document.getElementById('book-cancle');
+const openForm = document.getElementById("open-form");
+const closeForm = document.getElementById("close-form");
+const bookCancle = document.getElementById("book-cancle");
 
+const bookSubmit = document.getElementById("book-submit");
 
-openForm.addEventListener('click', function () {
-    document.getElementById('overlay-form').style.display = 'flex';
+openForm.addEventListener("click", function () {
+    document.getElementById("overlay-form").style.display = "flex";
 });
 
-closeForm.addEventListener('click', function () {
-    document.getElementById('overlay-form').style.display = 'none'
+closeForm.addEventListener("click", function () {
+    document.getElementById("overlay-form").style.display = "none";
 });
 
-bookCancle.addEventListener('click', function () {
-    document.getElementById('overlay-form').style.display = 'none'
+bookCancle.addEventListener("click", function () {
+    document.getElementById("overlay-form").style.display = "none";
+});
+
+bookSubmit.addEventListener("click", function () {
+    Swal.fire("Success!", "Berhasil ditambahkan", "success");
 });
